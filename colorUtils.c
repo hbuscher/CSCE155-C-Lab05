04b37e14e190cd5299a7cfe13ade6e86a1e9b82c
Henry <henryb2533@gmail.com>
2019-09-24 12:49:43 -0500

    Update colorUtils.c

--- colorUtils.c
+++ colorUtils.c
@@ -1,4 +1,4 @@
-
+#include <math.h>
 
 int max(int x, int y, int z) {
   int m = x > y ? x : y;
@@ -6,6 +6,32 @@ int max(int x, int y, int z) {
   return m;
 }
 
+int min(int x, int y, int z) {
+  int m = x < y ? x : y;
+  m = m < z ? m : z;
+  return m;
+}
+
 int toGrayScaleAverage(int r, int g, int b) {
   return (r + g + b) / 3;
+}
+
+int toGrayScaleLightness(int r, int g, int b) {
+  return round((max(r, g, b) + min(r, g, b)) / 2);
+}
+
+int toGrayScaleLuminosity(int r, int g, int b) {
+  return round(0.21 * r + 0.72 * g + 0.07 * b);
+}
+
+int toSepiaRed(int r, int g, int b) {
+  return round(0.393 * r + 0.769 * g + 0.189 * b);
+}
+
+int toSepiaGreen(int r, int g, int b) {
+  return round(0.349 * r + 0.686 * g + 0.168 * b);
+}
+
+int toSepiaBlue(int r, int g, int b) {
+  return round(0.272 * r + 0.534 * g + 0.131 * b);
 }
